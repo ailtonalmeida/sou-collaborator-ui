efdd1ebbe02a730833d8b532e7af474033ceceba
ailtonalmeida <ailton.reis@connectcom.univesp.br>
2019-01-28 14:09:17 -0200

    Revert "projeto sou colaborador"
    This reverts commit 99d7e6ff9c46410b9c3bc668542367235b8ffc00.

--- src/components/Container/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.main`
-  position: relative;
-  width: 100%;
-  min-height: calc(100vh - 210px);
-  height: 100%;
-  grid-area: content;
-`;
